bb7ce7df82062e1fbd637cc1142af85a132d19a9
BugHunter-X <askzenai@gmail.com>
2025-02-11 16:26:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+    setIntervalId(id);
+    return () => clearInterval(id);
+  }, []);
+
+  return <div>Count: {count}</div>;
+}
+```
